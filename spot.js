3a11bd24d96183a994858ed09bc71303f8fa7501
SimpleAutumn <36752805+SimpleAutumn@users.noreply.github.com>
2018-06-24 13:19:06 +0800

    更改

--- spot.js
+++ spot.js
@@ -6,17 +6,15 @@
     var engine = new Shape.Engine(canvas);
     var promise = new Promise((resolve) => { resolve(); });
     
-    document.getElementById('go').addEventListener('click', start);
     
     function start() {
-        document.getElementById('go').removeEventListener('click', start);
+       
         promise.then(() => engine.toText('I'))
         .then(() => engine.shake())
         .then(() => engine.toText('LOVE'))
         .then(() => engine.shake())
         .then(() => engine.toText('YOU'))
         .then(() => engine.shake())
-        .then(() => engine.clear())
-        .then(() => document.getElementById('go').addEventListener('click', start));
+        .then(() => engine.clear());
     }
 })();

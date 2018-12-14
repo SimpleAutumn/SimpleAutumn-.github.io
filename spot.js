76566503f4935f4eff85162a87aea348db342ad4
SimpleAutumn <451250135@qq.com>
2018-12-15 07:45:00 +0800

    更改

--- spot.js
+++ spot.js
@@ -7,10 +7,26 @@
     var promise = new Promise((resolve) => { resolve(); });
     document.getElementById('go').addEventListener('click', start);
     function start() {
-       document.getElementById('go').addEventListener('click', start);
-        promise.then(() => engine.toText('I'))
+        document.getElementById('go').removeEventListener('click', start);
+        promise.then(() => engine.toText('生'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('日'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('快'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('乐'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('天'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('天'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('开'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('心'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('(づ｡◕ᴗᴗ◕｡)づ'))
         .then(() => engine.shake())
         .then(() => engine.clear())
-		.then(() => document.getElementById('go').addEventListener('click', start));
+        .then(() => document.getElementById('go').addEventListener('click', start));
     }
 })();
